214aee249eff312225780f30fc624b47f9647624
maxu7xp <Maximiliangelgi11@gmail.com>
2024-11-12 09:04:50 +0100

    finally starting to get forms lol

--- mysit/myapp/views.py
+++ mysit/myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.forms import ContactForm
+from django.shortcuts import render
 
-def contact():
-    return HttpResponse
+def contact(request):
+    form = ContactForm
+    return render(request, 'form.html', {'form': form})
