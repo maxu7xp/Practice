59f6311231507f089fa39a0b994bb567772dfb34
maxu7xp <Maximiliangelgi11@gmail.com>
2024-11-12 06:58:38 +0100

    deleted and a lot of hassle now im trying to make a form for later use
    also really nice helps with less stress and easier production when building an app to be able to go back!

--- mysit/myapp/apps.py
+++ mysit/myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CatalogConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalog'
+    name = 'myapp'
