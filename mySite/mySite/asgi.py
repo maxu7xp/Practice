1ca52e5ad8bff43e31a686a72714eeeb8e67b35d
maxu7xp <Maximiliangelgi11@gmail.com>
2024-11-22 17:55:37 +0100

    deleted old files new proj time
    also create a new django start site and imma try to implement login  features thus needing forms and models and needing them interconnected, just did a lot of cybersec stuff so now i understand djangos design principles better also did a lot of reading on what the principles where and why and how to implement them. thus not i got a better understanding of them, also did a lot of research into http, and different protocols and readings about some vulnerabilities and how to secure my site better.
    
    now i just got a better understanding of django in general and of backend development in general.
    
    and of some of the features in django. that are implemented but i didnt know why or how they worked like cors and cross site scripting

--- mySite/mySite/asgi.py
+++ mySite/mySite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mysit project.
+ASGI config for mySite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySite.settings')
 
 application = get_asgi_application()
--- mysit/myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Snippet(models.Model):
-    name = models.CharField(max_length=100)
-    body = models.TextField()
-    
-    def __str__(self):
-        return self.name
-    
