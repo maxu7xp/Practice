4f2e19905c42d955813cd4211277a7cebd0e4109
maxu7xp <Maximiliangelgi11@gmail.com>
2024-11-23 05:30:35 +0100

    deleted it all because i found out of the auth api/ app

--- mySite/login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class registerModel(models.Model):
-    email = models.EmailField(max_length=254)
-    username = models.CharField(max_length=15)
-    password = models.CharField(max_length=20)
-    repeatpassword = models.CharField(max_length=20)
-    
-    
