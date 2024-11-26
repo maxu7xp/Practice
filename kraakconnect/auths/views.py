15f7bd0aca316095873274eba736b77e27411999
maxu7xp <Maximiliangelgi11@gmail.com>
2024-11-26 07:52:05 +0100

    yayaya made the login and it works made it using a form and just inputting it into the html and now i know that in the html <form> tag its there where you put the method="post"  and that you dont add a button but a <submit> thing its also a button but it loads it towards the formhandler!

--- kraakconnect/auths/views.py
+++ kraakconnect/auths/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.contrib.auth import login, logout, authenticate
 from .forms import loginForm
+from django.contrib.auth.forms import UserCreationForm
 
 def succes(request):
     return render(request, "authTemplates/success.html")
@@ -15,12 +16,18 @@ def mylogin(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                redirect('success')
+                return redirect('success')
             
             else:
-                raise Http404
+                print("ye")
+                return render(request, "authTemplates/login.html", {"form": form})
 
     else:
         form = loginForm()
     
     return render(request, "authTemplates/login.html", {"form": form})
+
+def Myregister(request):
+    form = UserCreationForm
+    return render(request, "authTemplates/register.html"), {"form": form}
+
