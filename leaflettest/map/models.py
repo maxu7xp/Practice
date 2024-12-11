524dc60dc247c70f2873ee462c22e7ae15f4c6b8
maxu7xp <Maximiliangelgi11@gmail.com>
2024-12-11 02:37:01 +0100

    okay nu die nieuwe model gemaakt voor de long en latitude en de type huizen
    ohja en ik kan gewoon de datapullen uit de database en neer zetten in de html! en het zoals dat opvullen

--- leaflettest/map/models.py
+++ leaflettest/map/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class MapLocationMarkerss(models.Model):
+    IsHuis = models.FloatField()
+    isKantoor = models.FloatField()
+    IsRave = models.FloatField()
+    MarkerLatitude = models.FloatField()
+    MarkerLongtitude = models.FloatField()
